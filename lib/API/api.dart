e4f7a5b1dde3befd690ce0bee0ff213e915ae8c7
samwel-chege <samwelchegeh09@gmail.com>
2023-02-15 23:25:17 +0300

    implement sign up

---
+++ lib/API/api.dart
@@ -0,0 +1,9 @@
+class Api {
+  static String apiUrl = "https://8c50-41-80-113-189.eu.ngrok.io/";
+
+  static String endpointSignup = "api/signup";
+  static String endpointLogin = "api/login";
+  static String endpointProducts = "api/products";
+
+  static int codeOK = 200;
+}
